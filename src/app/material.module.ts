48e4f94cbefdc5e2e9b4b84c21b51dc68276914e
Shivam Singh <shivamcena07@gmail.com>
2020-08-04 23:08:59 +0530

    Second Commit

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 @NgModule({
-    imports: [MatBadgeModule, MatButtonModule],
-    exports: [MatBadgeModule, MatButtonModule]
+    imports: [MatBadgeModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatTabsModule],
+    exports: [MatBadgeModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatTabsModule]
 })
 
 export class MaterialModule {}
